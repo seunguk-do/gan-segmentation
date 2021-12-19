9b7865a41b73e98ab92d1bf2ec919fef199bc457
sudo2000 <hu556727@gmail.com>
2021-12-19 06:33:00 +0000

    Edit evaluator

--- eval_utils.py
+++ eval_utils.py
@@ -4,33 +4,33 @@ import torch
 
 class mIoUEstimator(nn.Module):
     def __init__(self, n_classes=91):
-        super(mIoUEstimator, self).__init__()
+        super(mIoU, self).__init__()
         self.n_classes = n_classes
 
     def forward(self, inputs, targets, smooth=1):
-        """
-        Args: 
-            inputs: torch tensor [B, H, W] or [B, 1, H, W]
-            targets: torch tensor [B, H, W] or [B, 1, H, W]
-        """
-        B = inputs.shape[0]
-        inputs = inputs.view(B, -1)
-        targets = targets.view(B, -1)
-                
-        sum_IoU = 0
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
         
-        for i in range(self.n_classes):
+        ones = torch.ones_like(inputs)
+        
+        sum_IoU = 0
+        num_classes = 0
+        
+        for i in range(0, self.n_classes):
             a = torch.zeros_like(inputs)
             b = torch.zeros_like(inputs)
 
             a[torch.where(inputs == i)] = 1.
             b[torch.where(targets == i)] = 1.
         
-            intersection = (a * b).sum(dim=1)
-            total = (a + b).sum(dim=1)
+            intersection = (a * b).sum()
+            total = (a + b).sum()
             union = total - intersection 
         
-            IoU = (intersection + smooth)/(union + smooth)
-            sum_IoU += IoU
+            if union != 0:
+                IoU = (intersection + smooth)/(union + smooth)
+                sum_IoU += IoU
+                num_classes += 1
                 
-        return sum_IoU/(self.n_classes)
+                
+        return sum_IoU/(num_classes)
